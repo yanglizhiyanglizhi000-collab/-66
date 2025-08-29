23c84c7bc559b141a0488a9bea4413f278bbbbd3
yanglizhiyanglizhi000-collab <yanglizhiyanglizhi000@gmail.com>
2025-08-29 10:07:04 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,22 @@
+--名称
+appname="荔枝"
+--版本号
+appver="1.0"
+--版本
+appcode="1"
+--SDK
+appsdk="15"
+--包名
+packagename="com.mycompany.myapp1"
+--调试模式
+debugmode=false
+--应用权限
+user_permission={
+  "INTERNET",
+  "SYSTEM_ALERT_WINDOW",
+  "WRITE_EXTERNAL_STORAGE",
+}
+--跳过编译
+skip_compilation={
+
+}
